4d6d8ed6e3d9593bc844df78af543e925a018c1a
Reni <103267773+ReniIrinyi@users.noreply.github.com>
2023-08-02 20:55:28 +0200

    adding new blogpost

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { Routes, RouterModule } from "@angular/router";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { HeaderComponent } from "./components/1-Header/header/header.component";
 import { HomeComponent } from "./components/2-Home/home/home.component";
