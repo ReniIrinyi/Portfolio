086e60244ff35f4c47314824e3645969a414a631
Reni <103267773+ReniIrinyi@users.noreply.github.com>
2023-08-02 12:51:17 +0200

    fix some bugs

--- src/app/Model/BlogPost.ts
+++ src/app/Model/BlogPost.ts
@@ -2,8 +2,11 @@ export interface BlogPost {
   id: number;
   date: Date;
   name: string;
+  urlDescription?: string; // Optional property
   url?: string; // Optional property
+  url2Description?: string; // Optional property
   url2?: string; // Optional property
+  url3Description?: string; // Optional property
   url3?: string; // Optional property
   snippet1?: string; // Optional property
   snippet2?: string; // Optional property
