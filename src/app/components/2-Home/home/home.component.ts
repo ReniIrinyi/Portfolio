909ec7ac1f2583f8ea23378dd93df4382bddef9b
Reni <103267773+ReniIrinyi@users.noreply.github.com>
2024-03-13 20:35:14 +0100

    update vol2

--- src/app/components/2-Home/home/home.component.ts
+++ src/app/components/2-Home/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from "@angular/core";
+import { Component } from "@angular/core";
 
 declare const particlesJS: any;
 
@@ -7,15 +7,8 @@ declare const particlesJS: any;
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnInit {
-  constructor(private elementRef: ElementRef) {}
-  ngOnInit(): void {
-    console.log("home init");
-    this.loadParticlesFromLibrary();
-  }
-
-  // Load particles using particlesJS library
-  private loadParticlesFromLibrary(): void {
+export class HomeComponent {
+  constructor() {
     particlesJS.load("particles-js", "../../../../assets/particles.json", null);
   }
 }
